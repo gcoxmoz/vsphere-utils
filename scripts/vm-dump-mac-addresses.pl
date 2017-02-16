2536eaa8d0ebf75acc86581b7d572839a73516cd
Greg Cox <gcox@mozilla.com>
2017-02-16 00:38:52 +0000

    Include JSON export for MAC addresses

--- scripts/vm-dump-mac-addresses.pl
+++ scripts/vm-dump-mac-addresses.pl
@@ -13,7 +13,9 @@ $Util::script_version = '1.0';
 # with a MAC address.  Dump all the addresses, and grep away.
 #
 
+use JSON;
 my %opts = (
+    'json' => { type => '',   help => 'Export listing as JSON', required => 0},
   );
 
 # read/validate options and connect to the server
@@ -23,6 +25,7 @@ Opts::validate();
 
 # connect to the server
 Util::connect(); #print "Server Connected\n";
+my @output = ();
 searchMACs();
 # disconnect from the server
 Util::disconnect(); #print "Server Disconnected\n";
@@ -32,17 +35,28 @@ sub searchMACs {
     if (!$vms) { Util::trace(0, "No vms found '\n"); return; }
 
     foreach my $vm (sort { $a->name cmp $b->name } @$vms) {
-      my $vm_name = $vm->name;
+        my $vm_name = $vm->name;
 
-      my $hwlist_ref = $vm->get_property('config.hardware.device');
-      if (scalar(@{$hwlist_ref}) < 1) {
-        print "### VM '$vm_name' doesn't have a network(?)\n";
-        next;
-      }
-      foreach my $dev (@{$hwlist_ref}) {
-        next unless (ref($dev)->isa('VirtualEthernetCard'));
-        print '"'. $vm_name . '" ' . $dev->macAddress . ' ' . $dev->addressType . "\n";
-      }
+        my $hwlist_ref = $vm->get_property('config.hardware.device');
+        if (scalar(@{$hwlist_ref}) < 1) {
+            print "### VM '$vm_name' doesn't have a network(?)\n";
+            next;
+        }
+        foreach my $dev (@{$hwlist_ref}) {
+            next unless (ref($dev)->isa('VirtualEthernetCard'));
+            if (Opts::get_option('json')) {
+                my $json_blob; %{$json_blob} = ();
+                $json_blob->{'vm'}          = $vm_name;
+                $json_blob->{'macAddress'}  = $dev->macAddress;
+                $json_blob->{'addressType'} = $dev->addressType;
+                push @output, $json_blob;
+            } else {
+                print '"'. $vm_name . '" ' . $dev->macAddress . ' ' . $dev->addressType . "\n";
+            }
+        }
     }
 
+    if (Opts::get_option('json')) {
+        print to_json(\@output, {utf8 => 1, pretty => 1, canonical => 1, });
+    }
 }
