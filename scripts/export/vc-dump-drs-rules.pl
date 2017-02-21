103a31367199886c26896505946f2fec5fe8c93f
Greg Cox <gcox@mozilla.com>
2017-02-21 22:35:25 +0000

    Add hostgroup/vmgroups to dump and imports.

--- scripts/export/vc-dump-drs-rules.pl
+++ scripts/export/vc-dump-drs-rules.pl
@@ -15,6 +15,8 @@ $Util::script_version = '1.0';
 use JSON;
 my %opts = (
     'clusterName' => { type => '=s', help => 'Cluster name', required => 0},
+    'groups'      => { type => '!',  help => 'Use --nogroups not to show groups', default => 1, required => 0},
+    'vms'         => { type => '!',  help => 'Use --novms    not to show VMs',    default => 1, required => 0},
     'json'        => { type => '',   help => 'Export listing as JSON', required => 0},
     );
 
@@ -37,33 +39,57 @@ sub searchRules {
     if (!@{$clusters_ref}) { Util::trace(0, "ComputeResource '" . $clusterName . "' not found\n"); return; }
 
     foreach my $cluster_ref (sort { $a->name cmp $b->name } @{$clusters_ref}) {
-      my $rules = $cluster_ref->configurationEx->rule;
-      if (!$rules) { Util::trace(0, "Warning: No rules found for ComputeResource '" . $cluster_ref->name . "'\n"); }
-
-      next unless ($rules);
-      foreach my $rule (sort { $a->name cmp $b->name } @{$rules}) {
-          my $json = {};
-          my $rule_name = $rule->name;
-          $json->{'cluster'} = $cluster_ref->name;
-          $json->{'name'}    = $rule_name;
-          if ($rule->isa('ClusterAntiAffinityRuleSpec')) {
-              $json->{'type'} = 'ClusterAntiAffinityRuleSpec';
-              my @vms = sort map { $_->{'config.name'} }  @{ Vim::get_views( mo_ref_array => $rule->vm , properties => ['config.name'], ) };
-              $json->{'vms'} = \@vms;
-          } elsif ($rule->isa('ClusterAffinityRuleSpec')) {
-              $json->{'type'} = 'ClusterAffinityRuleSpec';
-              my @vms = sort map { $_->{'config.name'} }  @{ Vim::get_views( mo_ref_array => $rule->vm , properties => ['config.name'], ) };
-              $json->{'vms'} = \@vms;
-          } elsif ($rule->isa('ClusterVmHostRuleInfo')) {
-              $json->{'type'} = 'ClusterVmHostRuleInfo';
-              $json->{'affineHostGroupName'} = $rule->affineHostGroupName if ($rule->affineHostGroupName);
-              $json->{'antiAffineHostGroupName'} = $rule->antiAffineHostGroupName if ($rule->antiAffineHostGroupName);
-              $json->{'vmGroupName'} = $rule->vmGroupName if ($rule->vmGroupName);
-          } else {
-              print "### Rule '$rule_name' isn't of a type I understand.\n";
+      my $groups = $cluster_ref->configurationEx->group;
+      if (defined($groups) && Opts::get_option('groups')) {
+          foreach my $group (sort { $a->name cmp $b->name } @{$groups}) {
+              my $json = {};
+              my $group_name = $group->name;
+              $json->{'cluster'} = $cluster_ref->name;
+              $json->{'name'}    = $group_name;
+              if ($group->isa('ClusterHostGroup')) {
+                  next unless (defined($group->host));
+                  $json->{'type'} = 'ClusterHostGroup';
+                  my @hosts = sort map { $_->{'name'} }  @{ Vim::get_views( mo_ref_array => $group->host , properties => ['name'], ) };
+                  $json->{'hosts'} = \@hosts;
+              } elsif ($group->isa('ClusterVmGroup')) {
+                  $json->{'type'} = 'ClusterVmGroup';
+                  next unless (defined($group->vm));
+                  my @vms = sort map { $_->{'config.name'} }  @{ Vim::get_views( mo_ref_array => $group->vm , properties => ['config.name'], ) };
+                  $json->{'vms'} = \@vms;
+              } else {
+                  print "### Group '$group_name' isn't of a type I understand.\n";
+              }
+              my $json_blob; %{$json_blob} = %$json;
+              push @output, $json_blob;
           }
-          my $json_blob; %{$json_blob} = %$json;
-          push @output, $json_blob;
+      }
+
+      my $rules = $cluster_ref->configurationEx->rule;
+      if (defined($rules) && Opts::get_option('vms')) {
+          foreach my $rule (sort { $a->name cmp $b->name } @{$rules}) {
+              my $json = {};
+              my $rule_name = $rule->name;
+              $json->{'cluster'} = $cluster_ref->name;
+              $json->{'name'}    = $rule_name;
+              if ($rule->isa('ClusterAntiAffinityRuleSpec')) {
+                  $json->{'type'} = 'ClusterAntiAffinityRuleSpec';
+                  my @vms = sort map { $_->{'config.name'} }  @{ Vim::get_views( mo_ref_array => $rule->vm , properties => ['config.name'], ) };
+                  $json->{'vms'} = \@vms;
+              } elsif ($rule->isa('ClusterAffinityRuleSpec')) {
+                  $json->{'type'} = 'ClusterAffinityRuleSpec';
+                  my @vms = sort map { $_->{'config.name'} }  @{ Vim::get_views( mo_ref_array => $rule->vm , properties => ['config.name'], ) };
+                  $json->{'vms'} = \@vms;
+              } elsif ($rule->isa('ClusterVmHostRuleInfo')) {
+                  $json->{'type'} = 'ClusterVmHostRuleInfo';
+                  $json->{'affineHostGroupName'} = $rule->affineHostGroupName if ($rule->affineHostGroupName);
+                  $json->{'antiAffineHostGroupName'} = $rule->antiAffineHostGroupName if ($rule->antiAffineHostGroupName);
+                  $json->{'vmGroupName'} = $rule->vmGroupName if ($rule->vmGroupName);
+              } else {
+                  print "### Rule '$rule_name' isn't of a type I understand.\n";
+              }
+              my $json_blob; %{$json_blob} = %$json;
+              push @output, $json_blob;
+          }
       }
 
     }
--- scripts/export/vc-import-drs-rules.pl
+++ scripts/export/vc-import-drs-rules.pl
@@ -23,101 +23,168 @@ Opts::parse();
 Opts::validate();
 
 my $rulesfile = Opts::get_option('rulesfile');
-my @rules = ();
+my @inputs = ();
 if (!$rulesfile || ! -f $rulesfile) {
     die "Privileges file not provided.\n";
 } else {
     open( FH, '<'.$rulesfile ) or die "Can't open JSON file $rulesfile: $!\n";
     my $json_text = join '', <FH>;
     close FH;
-    my $rules_ref = from_json($json_text);
-    @rules = @$rules_ref;
+    my $inputs_ref = from_json($json_text);
+    @inputs = @$inputs_ref;
 }
 
 # connect to the server
 Util::connect(); #print "Server Connected\n";
-importRules();
+importFile();
 # disconnect from the server
 Util::disconnect(); #print "Server Disconnected\n";
 
-sub importRules {
-    RULE: foreach my $rule_ref (@rules) {
-        my $cluster_name = $rule_ref->{'cluster'};
-        my $rule_name    = $rule_ref->{'name'};
-        my $rule_type    = $rule_ref->{'type'};
-        my $newrule      = undef;
+sub importFile {
+    INPUT: foreach my $input_ref (@inputs) {
+        my $cluster_name = $input_ref->{'cluster'};
+        my $input_type    = $input_ref->{'type'};
+        my $input_name    = $input_ref->{'name'};
         my $cluster_ref = Vim::find_entity_view(view_type => 'ClusterComputeResource', filter => { name => $cluster_name } );
         if (!$cluster_ref) {
-            print "Could not find a cluster named $cluster_name.  Not applying rule $rule_name.\n";
-            next RULE;
+            print "Could not find a cluster named '$cluster_name'.  Not applying input line '$input_name'.\n";
+            next INPUT;
+        } elsif (! $input_type) {
+            print STDERR "$input_name did not have a 'type' attribute declared.\n";
+            next INPUT;
         }
-        my $existing_rules = $cluster_ref->configurationEx->rule;
-        if ($existing_rules) {
-            foreach my $oldrule (@{$existing_rules}) {
-                if ($oldrule->name eq $rule_name) {
-                    print "### There is already a rule on $cluster_name named $rule_name.  Skipping.\n";
-                    next RULE;
-                }
-            }
-        }
-        if (! $rule_type) {
-            print STDERR "$rule_name did not have a 'type' attribute declared.\n";
-            next RULE;
-        } elsif (($rule_type eq 'ClusterAntiAffinityRuleSpec') || ($rule_type eq 'ClusterAffinityRuleSpec')) {
-            my $vms_ref = $rule_ref->{'vms'};
-                 if ($rule_type eq 'ClusterAntiAffinityRuleSpec') {
-                $newrule = ClusterAntiAffinityRuleSpec->new('enabled' => 1, 'name' => $rule_name, );
-            } elsif ($rule_type eq 'ClusterAffinityRuleSpec') {
-                $newrule = ClusterAffinityRuleSpec->new(    'enabled' => 1, 'name' => $rule_name, );
-            }
-            my @vm_mors = ();
-            foreach my $vm_name (@$vms_ref) {
-                my $vm_mor = Vim::find_entity_view(view_type => 'VirtualMachine', filter => { name => $vm_name } );
-                if (!$vm_mor) {
-                    print STDERR "Did not find a VM named $vm_name on $cluster_name.  Skipping $rule_name.\n";
-                    next RULE;
-                }
-                push @vm_mors, $vm_mor;
-            }
-            $newrule->{'vm'} = [ @vm_mors ];
-        } elsif ($rule_type eq 'ClusterVmHostRuleInfo') {
-            $newrule = ClusterVmHostRuleInfo->new('enabled' => 1, 'name' => $rule_name, );
-            my $existing_groups = $cluster_ref->configurationEx->group;
-            if ($existing_groups) {
-                foreach my $attr ('affineHostGroupName', 'antiAffineHostGroupName', 'vmGroupName', ) {
-                    next unless (defined($rule_ref->{$attr}));
-                    my ($found_group) = grep { $_->name eq $rule_ref->{$attr} } @{$existing_groups};
-                    if ($found_group) {
-                        $newrule->{$attr} = $rule_ref->{$attr};
-                    } else {
-                        print STDERR "$cluster_name does not have a ".$rule_ref->{$attr}." group for $rule_name to use.\n";
-                        next RULE;
+
+        if (($input_type eq 'ClusterAntiAffinityRuleSpec') || ($input_type eq 'ClusterAffinityRuleSpec') || ($input_type eq 'ClusterVmHostRuleInfo')) {
+            my $existing_rules = $cluster_ref->configurationEx->rule;
+            if ($existing_rules) {
+                foreach my $oldrule (@{$existing_rules}) {
+                    if ($oldrule->name eq $input_name) {
+                        print "### There is already a rule on $cluster_name named $input_name.  Skipping.\n";
+                        next INPUT;
                     }
                 }
+            }
+
+            my $newrule      = undef;
+            if (($input_type eq 'ClusterAntiAffinityRuleSpec') || ($input_type eq 'ClusterAffinityRuleSpec')) {
+                my $vms_ref = $input_ref->{'vms'};
+                     if ($input_type eq 'ClusterAntiAffinityRuleSpec') {
+                    $newrule = ClusterAntiAffinityRuleSpec->new('enabled' => 1, 'name' => $input_name, );
+                } elsif ($input_type eq 'ClusterAffinityRuleSpec') {
+                    $newrule = ClusterAffinityRuleSpec->new(    'enabled' => 1, 'name' => $input_name, );
+                }
+                my @vm_mors = ();
+                foreach my $vm_name (@$vms_ref) {
+                    my $vm_mor = Vim::find_entity_view(view_type => 'VirtualMachine', filter => { name => $vm_name } );
+                    if (!$vm_mor) {
+                        print STDERR "Did not find a VM named $vm_name on $cluster_name.  Skipping $input_name.\n";
+                        next INPUT;
+                    }
+                    push @vm_mors, $vm_mor;
+                }
+                $newrule->{'vm'} = [ @vm_mors ];
+            } elsif ($input_type eq 'ClusterVmHostRuleInfo') {
+                $newrule = ClusterVmHostRuleInfo->new('enabled' => 1, 'name' => $input_name, );
+                my $existing_groups = $cluster_ref->configurationEx->group;
+                if ($existing_groups) {
+                    foreach my $attr ('affineHostGroupName', 'antiAffineHostGroupName', 'vmGroupName', ) {
+                        next unless (defined($input_ref->{$attr}));
+                        my ($found_group) = grep { $_->name eq $input_ref->{$attr} } @{$existing_groups};
+                        if ($found_group) {
+                            $newrule->{$attr} = $input_ref->{$attr};
+                        } else {
+                            print STDERR "$cluster_name does not have a ".$input_ref->{$attr}." group for '$input_name' to use.\n";
+                            next INPUT;
+                        }
+                    }
+                } else {
+                    print STDERR "$cluster_name did not have any groups; can't apply '$input_name'.\n";
+                    next INPUT;
+                }
+    
             } else {
-                print STDERR "$cluster_name did not have any groups; can't apply $rule_name.\n";
-                next RULE;
+                print STDERR "$input_name had an unsupported 'type' '$input_type'.\n";
+                next INPUT;
+            }
+            next INPUT unless (defined $newrule);
+            my $ruleSpec = ClusterRuleSpec->new();
+               $ruleSpec->{'operation'} = ArrayUpdateOperation->new('add');
+               $ruleSpec->{'info'} = $newrule;
+            my $clusterSpec = new ClusterConfigSpecEx();
+               $clusterSpec->{'rulesSpec'} = [ $ruleSpec ];
+            eval {
+                $cluster_ref->ReconfigureComputeResource(spec => $clusterSpec, modify => 1, );
+                $cluster_ref->RefreshRecommendation();
+            };
+            if ($@) {
+                print STDERR "$cluster_name / $input_name failed: ";
+                print(($@->isa('SoapFault') ? $@->fault_string : $@) . "\n");
+            } else {
+                print STDERR "$cluster_name / $input_name successfully added.\n";
+            }
+        } elsif (($input_type eq 'ClusterHostGroup') || ($input_type eq 'ClusterVmGroup')) {
+            my $existing_groups = $cluster_ref->configurationEx->group;
+            if ($existing_groups) {
+                foreach my $oldgroup (@{$existing_groups}) {
+                    if ($oldgroup->name eq $input_name) {
+                        print "### There is already a group on $cluster_name named '$input_name'.  Skipping.\n";
+                        next INPUT;
+                    }
+                }
+            }
+
+            my $newgroup      = undef;
+                 if ($input_type eq 'ClusterHostGroup') {
+                         $newgroup = ClusterHostGroup->new('name' => $input_name, );
+                my $hosts_ref = $input_ref->{'hosts'};
+                my @host_mors = ();
+                foreach my $host_name (@$hosts_ref) {
+                    my $host_mor = Vim::find_entity_view(view_type => 'HostSystem', filter => { name => $host_name } );
+                    if (!$host_mor) {
+                        print STDERR "Did not find a host named $host_name on $cluster_name.  Skipping '$input_name'.\n";
+                        next INPUT;
+                    }
+                    push @host_mors, $host_mor;
+                }
+                $newgroup->{'host'} = [ @host_mors ];
+            } elsif ($input_type eq 'ClusterVmGroup') {
+                    $newgroup =      ClusterVmGroup->new(  'name' => $input_name, );
+                my $vms_ref = $input_ref->{'vms'};
+                my @vm_mors = ();
+                foreach my $vm_name (@$vms_ref) {
+                    my $vm_mor = Vim::find_entity_view(view_type => 'VirtualMachine', filter => { name => $vm_name } );
+                    if (!$vm_mor) {
+                        print STDERR "Did not find a VM named $vm_name on $cluster_name.  Skipping '$input_name'.\n";
+                        next INPUT;
+                    }
+                    push @vm_mors, $vm_mor;
+                }
+                $newgroup->{'vm'} = [ @vm_mors ];
+            } else {
+                print STDERR "$input_name had an unsupported 'type' $input_type.\n";
+                next INPUT;
+            }
+            next INPUT unless (defined $newgroup);
+            my $groupSpec = ClusterGroupSpec->new();
+               $groupSpec->{'operation'} = ArrayUpdateOperation->new('add');
+               $groupSpec->{'info'} = $newgroup;
+            my $clusterSpec = new ClusterConfigSpecEx();
+               $clusterSpec->{'groupSpec'} = [ $groupSpec ];
+            eval {
+                $cluster_ref->ReconfigureComputeResource(spec => $clusterSpec, modify => 1, );
+                $cluster_ref->RefreshRecommendation();
+            };
+            if ($@) {
+                print STDERR "$cluster_name / $input_name failed: ";
+                print(($@->isa('SoapFault') ? $@->fault_string : $@) . "\n");
+            } else {
+                print STDERR "$cluster_name / $input_name successfully added.\n";
             }
 
         } else {
-            print STDERR "$rule_name had an unsupported 'type' $rule_type.\n";
-            next RULE;
+            print STDERR "$input_name had an unsupported 'type' '$input_type'.\n";
+            next INPUT;
         }
-        next RULE unless (defined $newrule);
-        my $ruleSpec = ClusterRuleSpec->new();
-           $ruleSpec->{'operation'} = ArrayUpdateOperation->new('add');
-           $ruleSpec->{'info'} = $newrule;
-        my $clusterSpec = new ClusterConfigSpecEx();
-           $clusterSpec->{'rulesSpec'} = [ $ruleSpec ];
-        eval {
-            $cluster_ref->ReconfigureComputeResource(spec => $clusterSpec, modify => 1, );
-            $cluster_ref->RefreshRecommendation();
-        };
-        if ($@) {
-            print STDERR "$cluster_name / $rule_name failed: ";
-            print(($@->isa('SoapFault') ? $@->fault_string : $@) . "\n");
-        } else {
-            print STDERR "$cluster_name / $rule_name successfully added.\n";
-        }
+
     }
 }
